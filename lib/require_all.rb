829408a3808d6fb64fbeeb30f7bfb20186e12e67
Alan Franzoni <registrazioni@franzoni.eu>
2014-07-03 17:05:11 +0200

    Remove assumption
    For a plugin system, we are perfectly comfortable with the fact that a dir may be empty.

--- lib/require_all.rb
+++ lib/require_all.rb
@@ -81,9 +81,6 @@ module RequireAll
       end
     end
 
-    # If there's nothing to load, you're doing it wrong!
-    raise LoadError, "no files to load" if files.empty?
-
     if options[:method] == :autoload
       files.map! { |file| [file, File.expand_path(file)] }
       files.each do |file, full_path|
